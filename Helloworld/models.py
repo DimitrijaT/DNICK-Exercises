3eb22c7437aece3727f570a4d7feb4aae59566b0
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-05-30 21:16:57 +0200

    Migrations and Admin Panel

--- Helloworld/models.py
+++ Helloworld/models.py
@@ -11,26 +11,29 @@ class Author(models.Model):
     country = models.CharField(max_length=50)
     biography = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Publication(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     address_1 = models.CharField(max_length=50)
-    house_number = models.CharField(max_length=50)
+    house_number = models.CharField(max_length=10)
     city = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
+    country = models.CharField(max_length=10)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     isbn = models.CharField(max_length=17)
     content = models.TextField(null=True, blank=True)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
-    publication = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
 
 # Табелата во која што ќе ги имаме дефинирано моделот за публикација-автор
 # Тука се чуваат парови автор-публикација
 class PublicationAuthor(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
-    publication = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
